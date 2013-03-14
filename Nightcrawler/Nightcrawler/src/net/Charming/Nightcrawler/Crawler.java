154f7ce1b666c5022df2b8f5c361da8ebb570e13
ltseng <ltseng@ubuntu.(none)>
2013-03-13 23:28:12 -0400

    Finished with Nightcrawler!

--- Nightcrawler/Nightcrawler/src/net/Charming/Nightcrawler/Crawler.java
+++ Nightcrawler/Nightcrawler/src/net/Charming/Nightcrawler/Crawler.java
@@ -3,7 +3,11 @@
  */
 package net.Charming.Nightcrawler;
 
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,17 +16,31 @@ import org.jsoup.select.Elements;
 
 /**
  * @author ltseng
- *
+ * Class for crawling the Internet from a given base URL
  */
 public class Crawler {
 	private String url; 
 	private Document doc;
 	public HashMap<String,String> linkMap;
+	public BufferedWriter writer;
+	public FileWriter fwriter;
 	
-	
-	public Crawler(String u){
+	/**
+	 * Constructor for a Crawler object. File writing objects are created
+	 * automatically, given a filename.
+	 * @param u
+	 * @param fname
+	 */
+	public Crawler(String u, String fname){
 		this.setUrl(u);
 		this.linkMap = new HashMap <String,String>();
+		try{
+			this.fwriter = new FileWriter(fname);
+			this.writer = new BufferedWriter(this.fwriter);
+		}catch(Exception err){
+			System.out.println(err);
+		}
+		
 	}
 
 	public String getUrl() {
@@ -33,59 +51,103 @@ public class Crawler {
 		this.url = url;
 	}
 	
-	
 	/**
-	 * Uses Jsoup to pull HTML from the private url variable and
-	 * sets it to a Document object.
+	 * Recursive method for getting html from a given link and parsing the html
+	 * to find links, for which it will then get the html. This only gets the
+	 * html if the given recursion depth is equal to the max depth.
+	 * @param depth
+	 * @param url
+	 * @return
 	 */
-	public void getHTML(){
-		try {
-			this.setDoc(Jsoup.connect(url).get());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public void crawl(int depth, String url){
+		Document current_page = new Document("");
+		try{
+			current_page = getHTML(url);
+		} catch(Exception err){
+			System.out.println(err);
 		}
-	}
-
-	public void parseHTML(){
-		Element body = this.doc.body();
-		Elements links = body.getElementsByTag("a");
-		for (Element link: links){
-			if (link.hasAttr("href")){
-				String linkURL = link.attr("href");
-				if (linkURL.toCharArray()[0] == '/'){
-					try{
-						Document currentDoc = Jsoup.connect(this.getUrl() + linkURL).get();
-						this.linkMap.put(this.getUrl()+linkURL, currentDoc.toString());
-
-					}catch (IOException err){
-						err.getStackTrace();
-					}
-				}
-				else if (linkURL.toCharArray()[0] == '#'){
-					continue;
-				}
-				else{
-					try{
-						Document currentDoc = Jsoup.connect(linkURL).get();
-						this.linkMap.put(linkURL, currentDoc.toString());
-
-					}catch (IOException err){
-						err.getStackTrace();
-					}
+		ArrayList<String> next_links = new ArrayList<String>();
+		next_links = parseHTML(url, current_page);
+		if (depth==2){
+			//System.out.println("Current url is " + url);
+			this.linkMap.put(url, current_page.toString());
+			try{
+				this.writer.write ("Link is: " + url);
+				this.writer.newLine();
+				this.writer.write(current_page.toString());
+				this.writer.newLine();
+				this.writer.newLine();
+			}catch (Exception err){
+				System.out.println(err);
+			}
+		} else{
+			for (String link : next_links){
+				if (link != url){
+					crawl(depth+1,link);
 				}
 			}
 		}
 	}
 	
-	public void printMap(){
-		for (String url: this.linkMap.keySet()){
-			System.out.println(url);
-			System.out.print("\t");
-			System.out.print(this.linkMap.get(url).length());
-			System.out.println("");
-			
+	/**
+	 * Uses Jsoup to pull HTML from the private url variable and
+	 * sets it to a Document object. 
+	 */
+	public Document getHTML(String url_to_get) throws IOException{
+		Document current_doc = Jsoup.connect(url_to_get).get();
+		if (current_doc == null){
+			throw new IOException();
 		}
+		return current_doc;
+	}
+
+	/**
+	 * Given a document, find all the Elements with "a" and "href," construct
+	 * a new url, which may or may not contain the base url, and add links to
+	 * an ArrayList of string links.
+	 * @param url
+	 * @param currentDoc
+	 * @return
+	 */
+	public ArrayList<String> parseHTML(String url, Document currentDoc){
+		ArrayList<String> next_links = new ArrayList<String>();
+		String new_url="";
+		try{
+			Element body = currentDoc.body();
+
+			Elements links = body.getElementsByTag("a");
+			for (Element link: links){
+				
+				if (link.hasAttr("href")){
+					String linkURL = link.attr("href");
+					if (linkURL.length()>0){
+						if (linkURL.toCharArray()[0] == '/' || linkURL.toCharArray()[0] == '_'){
+							new_url = url+linkURL;
+						}
+						else if (linkURL.toCharArray()[0] == '.'){
+							new_url = url+linkURL.substring(1,linkURL.length());
+						}
+						else if (linkURL.toCharArray()[0] == '#'){
+							break;
+						}
+						else if (!linkURL.substring(0, 4).equals("http")){
+							new_url = url+linkURL;
+						}
+						else{
+							new_url = linkURL;
+						}
+						if (!this.linkMap.containsKey(new_url)){
+							next_links.add(new_url);
+						}else{
+							System.out.println("Duplicate url");
+						}
+					}
+				}
+			}
+		}catch(Exception err){
+			System.out.println(err.toString());
+		}
+		return next_links;
 	}
 	
 	public Document getDoc() {
--- Nightcrawler/Nightcrawler/src/net/Charming/Nightcrawler/TestCrawler.java
+++ Nightcrawler/Nightcrawler/src/net/Charming/Nightcrawler/TestCrawler.java
@@ -5,19 +5,19 @@ package net.Charming.Nightcrawler;
 import org.jsoup.nodes.Document;
 /**
  * @author ltseng and sbecht
- *
+ * Class for testing the Crawler class, which, given a base url,
+ * extracts the links from that html for recursive crawling.
  */
 public class TestCrawler {
 
 	/**
+	 * Instantiate a new Crawler object and write results to a given file.
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Crawler nightCrawler = new Crawler("http://students.olin.edu");
-		nightCrawler.getHTML();
-		nightCrawler.parseHTML();
-		nightCrawler.printMap();
+		Crawler nightCrawler = new Crawler("http://www.olin.edu", "Crawler.txt");
+		nightCrawler.crawl(1,nightCrawler.getUrl());
 	}
 
 }
